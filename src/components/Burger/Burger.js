c8cde958aaa1c4f498ac65d95889a969d0fa979e
Michael Tran <michaeltranx@gmail.com>
2019-08-16 13:52:26 -0500

    Semi-Dynamic Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,25 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient.js';
 
 const burger = (props) => {
+    // console.log(props.ingredients);
+    // console.log(Object.keys(props.ingredients));
+
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        // console.log(igKey)
+        // console.log(props.ingredients[igKey])
+        // console.log(...Array(props.ingredients[igKey]))
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            // console.log(igKey)
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+    });
+
+    console.log(transformedIngredients)
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
