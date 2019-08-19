5de18108c92dad3e0ebdf08b62047d630494b5a0
Michael Tran <michaeltranx@gmail.com>
2019-08-19 10:18:23 -0500

    SideDrawer - mobile friendly

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo.js';
+import NavigationItems from '../../Navigation/NavigationItems/NavagationItems.js';
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop.js';
+import Aux from '../../../hoc/Auxiliary.js';
+
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+};
+
+export default sideDrawer;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo.js';
+import NavigationItems from '../NavigationItems/NavagationItems.js';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle.js';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <div>LOGO</div>
-        <nav>
-            ...
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        {/* <div onClick={props.opened}>MENU</div> */}
+        <div className={classes.Logo}>
+            <Logo height="80%" />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
         </nav>
     </header>
 );
