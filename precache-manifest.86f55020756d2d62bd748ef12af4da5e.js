77ea34cddd315f120dc37417019ca24c54db8725
Michael Tran <michaeltranx@gmail.com>
2019-08-20 16:48:41 -0500

    Updates

---
+++ precache-manifest.86f55020756d2d62bd748ef12af4da5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310e61cc1ad899734e54601bc1fe8e26",
+    "url": "/React-Burger-Builder/index.html"
+  },
+  {
+    "revision": "2a757f533e8b061c380a",
+    "url": "/React-Burger-Builder/static/css/main.d7f3c871.chunk.css"
+  },
+  {
+    "revision": "46efc08397695f4834e3",
+    "url": "/React-Burger-Builder/static/js/2.4c2733bc.chunk.js"
+  },
+  {
+    "revision": "2a757f533e8b061c380a",
+    "url": "/React-Burger-Builder/static/js/main.d6d0f723.chunk.js"
+  },
+  {
+    "revision": "e4e593faaacd65465699",
+    "url": "/React-Burger-Builder/static/js/runtime~main.3b8df7e9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-Burger-Builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
