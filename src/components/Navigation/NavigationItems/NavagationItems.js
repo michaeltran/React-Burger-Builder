ae027910cfd77d16cb342f271be9a922d2128983
Michael Tran <michaeltranx@gmail.com>
2019-08-26 12:38:32 -0500

    Testing module

--- src/components/Navigation/NavigationItems/NavagationItems.js
+++ src/components/Navigation/NavigationItems/NavagationItems.js
@@ -7,7 +7,9 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         { props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null }
-        { !props.isAuthenticated ? <NavigationItem link="/auth">Authenticate</NavigationItem> : <NavigationItem link="/logout">Logout</NavigationItem> }
+        { !props.isAuthenticated 
+            ? <NavigationItem link="/auth">Authenticate</NavigationItem> 
+            : <NavigationItem link="/logout">Logout</NavigationItem> }
     </ul>
 );
 
